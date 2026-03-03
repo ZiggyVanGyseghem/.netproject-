c3bbdcf64e88e0fcd32cd0f3e629b5abbb96a9c6
CherrySpace6 <ziggy.vaagygehem@gmail.com>
2026-03-03 23:00:20 +0100

    yep

--- db/init/create_db.sql
+++ db/init/create_db.sql
@@ -7,209 +7,134 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+-- --------------------------------------------------------
 
-CREATE TABLE `allergens` (
+CREATE TABLE `bestellingen` (
   `id` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL
+  `gebruiker_id` int(11) NOT NULL,
+  `tijdstip_besteld` datetime NOT NULL,
+  `status` enum('InDeWachtrij','InBereiding','KlaarVoorOphalen','Afgeleverd') NOT NULL,
+  `betaalstatus` enum('Open','Betaald','Geannuleerd','Mislukt') NOT NULL,
+  `mollie_payment_id` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
-CREATE TABLE `allergen_presences` (
-  `allergen_id` int(11) NOT NULL,
-  `ingredient_id` int(11) NOT NULL
+CREATE TABLE `bestellijnen` (
+  `bestelling_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `hoeveelheid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
-CREATE TABLE `allergen_sensitivities` (
-  `child_id` int(11) NOT NULL,
-  `allergen_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
-CREATE TABLE `children` (
-  `user_id` int(11) NOT NULL,
-  `class_id` int(11) NOT NULL,
-  `food_preference` enum('meat','veggie','vegan') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
-CREATE TABLE `classes` (
+CREATE TABLE `gebruikers` (
   `id` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL
+  `naam` varchar(255) NOT NULL,
+  `wachtwoord_hash` varchar(255) DEFAULT NULL,
+  `unieke_code` varchar(255) DEFAULT NULL,
+  `tijdstip_geactiveerd` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
-CREATE TABLE `hot_meals` (
+CREATE TABLE `productdetails` (
+  `product_id` int(11) NOT NULL,
+  `tijdstip` datetime NOT NULL,
+  `naam` varchar(255) NOT NULL,
+  `prijs` float NOT NULL,
+  `producttype` enum('Drank','Versnapering') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+CREATE TABLE `producten` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+CREATE TABLE `rollen` (
   `id` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `recipe` varchar(255) NOT NULL
+  `naam` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
-CREATE TABLE `hot_meal_choices` (
-  `date` date NOT NULL,
-  `meal_choice_child_id` int(11) NOT NULL,
-  `hot_meal_id` int(11) NOT NULL
+CREATE TABLE `roltoewijzingen` (
+  `gebruiker_id` int(11) NOT NULL,
+  `rol_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
-CREATE TABLE `ingredients` (
+CREATE TABLE `tafels` (
   `id` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `type` enum('meat','veggie','vegan') NOT NULL,
-  `unit_of_measurement` enum('kg','l','') NOT NULL
+  `nummer` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
-CREATE TABLE `meal_choices` (
-  `date` date NOT NULL,
-  `child_id` int(11) NOT NULL,
-  `choice` enum('home','cold','hot') NOT NULL
+CREATE TABLE `tafeltoewijzingen` (
+  `gebruiker_id` int(11) NOT NULL,
+  `tafel_id` int(11) NOT NULL,
+  `tijdstip_toegewezen` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
-CREATE TABLE `meal_ingredients` (
-  `hot_meal_id` int(11) NOT NULL,
-  `ingredient_id` int(11) NOT NULL,
-  `quantity` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+-- --------------------------------------------------------
 
-CREATE TABLE `parental_relations` (
-  `parent_id` int(11) NOT NULL,
-  `child_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+ALTER TABLE `bestellingen`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk__bestellingen__gebruiker_id` (`gebruiker_id`);
 
-CREATE TABLE `parents` (
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+ALTER TABLE `bestellijnen`
+  ADD PRIMARY KEY (`bestelling_id`,`product_id`),
+  ADD KEY `fk__bestellijnen__product_id` (`product_id`);
 
-CREATE TABLE `scheduled_hot_meals` (
-  `date` date NOT NULL,
-  `hot_meal_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
-CREATE TABLE `staff` (
-  `user_id` int(11) NOT NULL,
-  `role` enum('kitchen','teaching','management') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
-CREATE TABLE `teachers` (
-  `staff_id` int(11) NOT NULL,
-  `class_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
-
-ALTER TABLE `allergens`
+ALTER TABLE `gebruikers`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `allergen_presences`
-  ADD PRIMARY KEY (`allergen_id`,`ingredient_id`),
-  ADD KEY `fk__allergen_presences__ingredient_id` (`ingredient_id`);
+ALTER TABLE `productdetails`
+  ADD PRIMARY KEY (`product_id`,`tijdstip`);
 
-ALTER TABLE `allergen_sensitivities`
-  ADD PRIMARY KEY (`child_id`,`allergen_id`),
-  ADD KEY `fk__allergen_sensitivities__allergen_id` (`allergen_id`);
-
-ALTER TABLE `children`
-  ADD PRIMARY KEY (`user_id`),
-  ADD KEY `fk__children__class_id` (`class_id`);
-
-ALTER TABLE `classes`
+ALTER TABLE `producten`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `hot_meals`
+ALTER TABLE `rollen`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `hot_meal_choices`
-  ADD PRIMARY KEY (`date`,`meal_choice_child_id`,`hot_meal_id`),
-  ADD KEY `fk__hot_meal_choices__hot_meal_id` (`hot_meal_id`),
-  ADD KEY `fk__hot_meal_choices__scheduled_hot_meal_composite` (`date`,`hot_meal_id`);
+ALTER TABLE `roltoewijzingen`
+  ADD PRIMARY KEY (`gebruiker_id`,`rol_id`),
+  ADD KEY `fk__roltoewijzingen__rol_id` (`rol_id`);
 
-ALTER TABLE `ingredients`
+ALTER TABLE `tafels`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `meal_choices`
-  ADD PRIMARY KEY (`date`,`child_id`),
-  ADD KEY `fk__meal_choices__child_id` (`child_id`);
+ALTER TABLE `tafeltoewijzingen`
+  ADD PRIMARY KEY (`gebruiker_id`,`tafel_id`,`tijdstip_toegewezen`),
+  ADD KEY `fk__tafeltoewijzingen__tafel_id` (`tafel_id`);
 
-ALTER TABLE `meal_ingredients`
-  ADD PRIMARY KEY (`hot_meal_id`,`ingredient_id`),
-  ADD KEY `fk__meal_ingredients__ingredient_id` (`ingredient_id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `parental_relations`
-  ADD PRIMARY KEY (`parent_id`,`child_id`),
-  ADD KEY `fk__parental_relations__child_id` (`child_id`);
-
-ALTER TABLE `parents`
-  ADD PRIMARY KEY (`user_id`);
-
-ALTER TABLE `scheduled_hot_meals`
-  ADD PRIMARY KEY (`date`,`hot_meal_id`),
-  ADD KEY `fk__scheduled_hot_meals__hot_meal_id` (`hot_meal_id`);
-
-ALTER TABLE `staff`
-  ADD PRIMARY KEY (`user_id`);
-
-ALTER TABLE `teachers`
-  ADD PRIMARY KEY (`staff_id`,`class_id`),
-  ADD KEY `fk__teachers__class_id` (`class_id`);
-
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
-
-ALTER TABLE `allergens`
+ALTER TABLE `bestellingen`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `classes`
+ALTER TABLE `gebruikers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `hot_meals`
+ALTER TABLE `producten`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `ingredients`
+ALTER TABLE `rollen`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `users`
+ALTER TABLE `tafels`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+-- --------------------------------------------------------
 
-ALTER TABLE `allergen_presences`
-  ADD CONSTRAINT `fk__allergen_presences__allergen_id` FOREIGN KEY (`allergen_id`) REFERENCES `allergens` (`id`),
-  ADD CONSTRAINT `fk__allergen_presences__ingredient_id` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`);
+ALTER TABLE `bestellingen`
+  ADD CONSTRAINT `fk__bestellingen__gebruiker_id` FOREIGN KEY (`gebruiker_id`) REFERENCES `gebruikers` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `allergen_sensitivities`
-  ADD CONSTRAINT `fk__allergen_sensitivities__allergen_id` FOREIGN KEY (`allergen_id`) REFERENCES `allergens` (`id`),
-  ADD CONSTRAINT `fk__allergen_sensitivities__child_id` FOREIGN KEY (`child_id`) REFERENCES `children` (`user_id`);
+ALTER TABLE `bestellijnen`
+  ADD CONSTRAINT `fk__bestellijnen__bestelling_id` FOREIGN KEY (`bestelling_id`) REFERENCES `bestellingen` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk__bestellijnen__product_id` FOREIGN KEY (`product_id`) REFERENCES `producten` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `children`
-  ADD CONSTRAINT `fk__children__class_id` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`),
-  ADD CONSTRAINT `fk__children__user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE `productdetails`
+  ADD CONSTRAINT `fk__productdetails__product_id` FOREIGN KEY (`product_id`) REFERENCES `producten` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `hot_meal_choices`
-  ADD CONSTRAINT `fk__hot_meal_choices__meal_choice_composite` FOREIGN KEY (`date`,`meal_choice_child_id`) REFERENCES `meal_choices` (`date`, `child_id`),
-  ADD CONSTRAINT `fk__hot_meal_choices__scheduled_hot_meal_composite` FOREIGN KEY (`date`,`hot_meal_id`) REFERENCES `scheduled_hot_meals` (`date`, `hot_meal_id`);
+ALTER TABLE `roltoewijzingen`
+  ADD CONSTRAINT `fk__roltoewijzingen__gebruiker_id` FOREIGN KEY (`gebruiker_id`) REFERENCES `gebruikers` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk__roltoewijzingen__rol_id` FOREIGN KEY (`rol_id`) REFERENCES `rollen` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `meal_choices`
-  ADD CONSTRAINT `fk__meal_choices__child_id` FOREIGN KEY (`child_id`) REFERENCES `children` (`user_id`);
+ALTER TABLE `tafeltoewijzingen`
+  ADD CONSTRAINT `fk__tafeltoewijzingen__gebruiker_id` FOREIGN KEY (`gebruiker_id`) REFERENCES `gebruikers` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk__tafeltoewijzingen__tafel_id` FOREIGN KEY (`tafel_id`) REFERENCES `tafels` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `meal_ingredients`
-  ADD CONSTRAINT `fk__meal_ingredients__hot_meal_id` FOREIGN KEY (`hot_meal_id`) REFERENCES `hot_meals` (`id`),
-  ADD CONSTRAINT `fk__meal_ingredients__ingredient_id` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`);
-
-ALTER TABLE `parental_relations`
-  ADD CONSTRAINT `fk__parental_relations__child_id` FOREIGN KEY (`child_id`) REFERENCES `children` (`user_id`),
-  ADD CONSTRAINT `fk__parental_relations__parent_id` FOREIGN KEY (`parent_id`) REFERENCES `parents` (`user_id`);
-
-ALTER TABLE `parents`
-  ADD CONSTRAINT `fk__parents__user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `scheduled_hot_meals`
-  ADD CONSTRAINT `fk__scheduled_hot_meals__hot_meal_id` FOREIGN KEY (`hot_meal_id`) REFERENCES `hot_meals` (`id`);
-
-ALTER TABLE `staff`
-  ADD CONSTRAINT `fk__staff__user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `teachers`
-  ADD CONSTRAINT `fk__teachers__class_id` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`),
-  ADD CONSTRAINT `fk__teachers__staff_id` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
